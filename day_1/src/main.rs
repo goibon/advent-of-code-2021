f7e781c42e031d80e16b0226ee3c6edd6209c320
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2021-12-10 18:34:31 +0100

    Added solution to day 1 part 2.

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -28,6 +28,28 @@ fn count_increments(input: &Vec<u16>) -> u16 {
     count
 }
 
+fn count_window_increments(input: &Vec<u16>) -> u16 {
+    let mut count: u16 = 0;
+    let mut windows = input.windows(3);
+
+    let mut previous_sum: u16 = 0;
+
+    if let Some(first_window) = windows.next() {
+        previous_sum = first_window.iter().sum();
+    }
+
+    while let Some(next_window) = windows.next() {
+        let current_sum = next_window.iter().sum();
+        if current_sum > previous_sum {
+            count += 1;
+        }
+
+        previous_sum = current_sum;
+    }
+
+    count
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
@@ -37,4 +59,7 @@ fn main() {
 
     let part_1 = count_increments(&input);
     println!("Part 1: {:?}", part_1);
+
+    let part_2 = count_window_increments(&input);
+    println!("Part 2: {:?}", part_2);
 }
