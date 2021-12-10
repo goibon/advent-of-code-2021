2f1ed191877252b00cd7c4af8d657dc7140ff666
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2021-12-10 20:33:22 +0100

    Added utils crate to contain shared functionality.

---
+++ utils/src/lib.rs
@@ -0,0 +1,8 @@
+use std::{fs::File, io::Read};
+
+pub fn read_file(file_path: &str) -> std::io::Result<String> {
+    let mut file = File::open(file_path)?;
+    let mut string = String::new();
+    file.read_to_string(&mut string)?;
+    Ok(string)
+}
