a810edd3425445048cc5051f5b04c2fadcfc9127
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2021-12-11 12:38:18 +0100

    Added solution to day 3 part 2.

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -23,6 +23,19 @@ fn create_column_vector(rows: &Vec<&str>) -> Vec<Vec<u32>> {
     columns
 }
 
+fn create_column_vector_for_specific_index(rows: &Vec<&str>, index: usize) -> Vec<u32> {
+    let mut column: Vec<u32> = Vec::new();
+
+    for &row in rows {
+        let char_at_index = row.chars().skip(index).take(1).collect::<Vec<char>>()[0];
+        if let Some(digit) = char_at_index.to_digit(2) {
+            column.push(digit);
+        }
+    }
+
+    column
+}
+
 fn get_most_common_bit(input: &Vec<u32>) -> char {
     let mut zero_count: u32 = 0;
     let mut one_count: u32 = 0;
@@ -71,6 +84,53 @@ fn solve_part_1(input: &Vec<&str>) {
     }
 }
 
+fn get_rating(input: &Vec<&str>, default_bit: char, use_least_common_bit: bool) -> u32 {
+    let mut remaining_numbers = input.clone();
+    let mut index: usize = 0;
+    while remaining_numbers.len() > 1 {
+        let column = create_column_vector_for_specific_index(&remaining_numbers, index);
+        let mut most_common_bit = get_most_common_bit(&column);
+
+        most_common_bit = if most_common_bit == '-' {
+            default_bit
+        } else if use_least_common_bit {
+            if most_common_bit == '1' {
+                '0'
+            } else {
+                '1'
+            }
+        } else {
+            most_common_bit
+        };
+
+        let mut new_remaining_numbers: Vec<&str> = Vec::new();
+        for number in remaining_numbers {
+            if let Some(char_at_index) = number.chars().skip(index).next() {
+                if char_at_index == most_common_bit {
+                    new_remaining_numbers.push(number);
+                }
+            }
+        }
+        remaining_numbers = new_remaining_numbers;
+        index += 1;
+    }
+
+    if let Ok(number) = usize::from_str_radix(remaining_numbers[0], 2) {
+        number as u32
+    } else {
+        0
+    }
+}
+
+fn solve_part_2(input: &Vec<&str>) {
+    let oxygen_generator_rating = get_rating(input, '1', false);
+    let co2_scrubber_rating = get_rating(input, '0', true);
+    println!(
+        "Part 2: {:?}",
+        oxygen_generator_rating * co2_scrubber_rating
+    );
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
@@ -78,4 +138,5 @@ fn main() {
     let input = utils::read_file(path).expect("Error reading file.");
     let input = split_input(&input);
     solve_part_1(&input);
+    solve_part_2(&input);
 }
