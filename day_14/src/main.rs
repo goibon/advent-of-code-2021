1a38bd431477c734f009e47f7852e1bb1d26d816
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2021-12-28 02:15:30 +0100

    Added solution to day 14 part 2.
    This solution ended up also working for part_1 so it also removes some now redundant functions.

--- day_14/src/main.rs
+++ day_14/src/main.rs
@@ -15,53 +15,91 @@ fn split_input(string: &str) -> (&str, HashMap<&str, char>) {
     (template, insertion_rules)
 }
 
-fn get_char_pairs_from_string(string: &str) -> Vec<String> {
+fn get_char_pairs_from_string(string: &str) -> Vec<(char, char)> {
     let mut char_pairs = Vec::new();
     let char_vector: Vec<char> = string.chars().collect();
-    for index in (1..string.len()).rev() {
-        let pair = char_vector[index - 1].to_string() + &char_vector[index].to_string();
-        char_pairs.push(pair);
+    for index in 1..string.len() {
+        char_pairs.push((char_vector[index - 1], char_vector[index]))
     }
 
     char_pairs
 }
 
-fn insert_into_template(template: &str, insertion_rules: &HashMap<&str, char>) -> String {
-    let char_pairs = get_char_pairs_from_string(template);
-    let mut new_template: Vec<char> = template.chars().collect();
+fn recurse_insertion_pair(
+    pair: &str,
+    insertion_rules: &HashMap<&str, char>,
+    depth: u32,
+    cached_results: &mut HashMap<(String, u32), HashMap<char, u64>>,
+) -> HashMap<char, u64> {
+    if depth == 0 {
+        let right_pair_char = pair.chars().collect::<Vec<_>>()[1];
+        return HashMap::from([(right_pair_char, 1)]);
+    }
 
-    for (index, char_pair) in char_pairs.iter().enumerate() {
-        if let Some(char_to_insert) = insertion_rules.get(char_pair.as_str()) {
-            let insertion_index = template.len() - 1 - index;
-            new_template.insert(insertion_index, *char_to_insert);
+    if let Some(cached_result) = cached_results.get(&(pair.to_string(), depth)) {
+        return cached_result.clone();
+    }
+
+    let mut result = HashMap::new();
+    if let Some(char_to_insert) = insertion_rules.get(pair) {
+        let left_pair_char = pair.chars().collect::<Vec<_>>()[0];
+        let right_pair_char = pair.chars().collect::<Vec<_>>()[1];
+        let new_left_pair = vec![left_pair_char, *char_to_insert]
+            .iter()
+            .collect::<String>();
+        let new_right_pair = vec![*char_to_insert, right_pair_char]
+            .iter()
+            .collect::<String>();
+
+        result = recurse_insertion_pair(&new_left_pair, insertion_rules, depth - 1, cached_results);
+        let second_count_map =
+            recurse_insertion_pair(&new_right_pair, insertion_rules, depth - 1, cached_results);
+
+        for key in second_count_map.keys() {
+            *result.entry(*key).or_default() += *second_count_map.get(key).unwrap();
+        }
+
+        cached_results
+            .entry((pair.to_owned(), depth))
+            .or_insert_with(|| result.clone());
+    }
+
+    result
+}
+
+fn count_chars_after_insertions(
+    template: &str,
+    insertion_rules: &HashMap<&str, char>,
+    depth: u32,
+) -> u64 {
+    let initial_pairs: Vec<Vec<char>> = get_char_pairs_from_string(template)
+        .iter()
+        .map(|(left, right)| vec![*left, *right])
+        .collect();
+    let mut result = String::new();
+    result.extend(template.chars().collect::<Vec<_>>().first());
+
+    let mut result_map: HashMap<char, u64> =
+        HashMap::from([(*template.chars().collect::<Vec<_>>().first().unwrap(), 1)]);
+    let mut cached_results: HashMap<(String, u32), HashMap<char, u64>> = HashMap::new();
+    for pair in initial_pairs {
+        let intermediate_result = recurse_insertion_pair(
+            &pair.iter().collect::<String>(),
+            insertion_rules,
+            depth,
+            &mut cached_results,
+        );
+
+        for key in intermediate_result.keys() {
+            *result_map.entry(*key).or_default() += *intermediate_result.get(key).unwrap();
         }
     }
 
-    new_template.into_iter().collect()
-}
-
-fn count_character_occurrences(string: &str) -> HashMap<char, u32> {
-    let mut character_counts = HashMap::new();
-
-    for character in string.chars().collect::<Vec<_>>() {
-        *character_counts.entry(character).or_insert(0) += 1;
-    }
-
-    character_counts
-}
-
-fn part_1(template: &str, insertion_rules: &HashMap<&str, char>) {
-    let mut template = template.to_string();
-    for _ in 1..=10 {
-        template = insert_into_template(&template, insertion_rules);
-    }
-    let character_counts = count_character_occurrences(&template);
-    let mut counts: Vec<u32> = character_counts.values().cloned().collect();
+    let mut counts: Vec<_> = result_map.values().collect();
     counts.sort_unstable();
     let least_frequent = counts.first().unwrap();
     let most_frequent = counts.last().unwrap();
-
-    println!("Part 1: {}", most_frequent - least_frequent);
+    *most_frequent - *least_frequent
 }
 
 fn main() {
@@ -70,5 +108,9 @@ fn main() {
 
     let input = std::fs::read_to_string(path).expect("Error reading file.");
     let (template, insertion_rules) = split_input(&input);
-    part_1(template, &insertion_rules);
+
+    let part_1 = count_chars_after_insertions(template, &insertion_rules, 10);
+    println!("Part 1: {}", part_1);
+    let part_2 = count_chars_after_insertions(template, &insertion_rules, 40);
+    println!("Part 2: {}", part_2);
 }
